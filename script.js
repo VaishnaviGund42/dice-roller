28bc02dfe33cd381fa60329b2b2bce5500f2658c
VaishnaviGund42 <vaishnavigund42@gmail.com>
2025-09-05 19:22:25 +0530

    create the files

---
+++ script.js
@@ -0,0 +1,10 @@
+function rollDice() {
+  // Step 1: Random number between 1 and 6
+  var randomNumber = Math.floor(Math.random() * 6) + 1;
+
+  // Step 2: Dice faces in Unicode (⚀ ⚁ ⚂ ⚃ ⚄ ⚅)
+  var diceFaces = ["\u2680", "\u2681", "\u2682", "\u2683", "\u2684", "\u2685"];
+
+  // Step 3: Update dice element
+  document.getElementById("dice").innerHTML = diceFaces[randomNumber - 1];
+}
